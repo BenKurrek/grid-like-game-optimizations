da47cdcf29b7f0aafd5dcc36beb061ec15b9edf0
BenKurrek <bkurrek@uwaterloo.ca>
2023-11-25 14:00:18 -0500

    added center pawn logic

--- src/game/chess/evaluations/pawn.py
+++ src/game/chess/evaluations/pawn.py
@@ -6,7 +6,7 @@ from chess import Square, Piece
 
 pawn_weight_labels = [
     "Pawn Material Weight",
-    "Pawn Islands Weight",
+    "Center Pawn Weight",
     "Pawn Chains Weight",
     "Passed Pawns Weight"
 ]
@@ -17,6 +17,9 @@ pawn_weight_bounds = [
     (0, 1000), # how much pawn chains are worth
     (0, 1000), # how much passed pawns are worth
 ]
+
+WHITE_SCORE_IDX = 0
+BLACK_SCORE_IDX = 1
 
 class PawnEvaluator:
     def __init__(self, board, weights):
@@ -39,80 +42,23 @@ class PawnEvaluator:
 
     def evaluation_for_square(self, square, piece):
         if piece.piece_type == chess.PAWN:
+            # If the square is a pawn, give it the material bonus
             self.material_evaluation(piece)
 
+            # If the square is in the center, give it a bonus
+            if square in chess.SquareSet(chess.BB_CENTER):
+                self.center_pawn_evaluation(piece)
+
     def material_evaluation(self, piece):
+        pawn_material_idx = 0
         if piece.color == chess.WHITE:
-            self.scores_for_weights[0][0] += self.weights[0]
+            self.scores_for_weights[pawn_material_idx][WHITE_SCORE_IDX] += self.weights[pawn_material_idx]
         else:
-            self.scores_for_weights[0][1] += self.weights[0]
+            self.scores_for_weights[pawn_material_idx][BLACK_SCORE_IDX] += self.weights[pawn_material_idx]
 
-    def calculate_pawn_islands(self, piece):
-        file = chess.square_file(square)
-        if file not in seen_files:
-            pawn_islands += 1
-            seen_files.add(file)
-
-        return pawn_islands
-
-    def calculate_pawn_chains(self, board):
-        # Count the number of pawn chains
-        pawn_chains = 0
-
-        for square in chess.SQUARES:
-            piece = board.piece_at(square)
-            if piece and piece.piece_type == chess.PAWN:
-                if self.is_pawn_chain(board, square):
-                    pawn_chains += 1
-
-        return pawn_chains
-
-    def is_pawn_chain(self, board, square):
-        # Check if the pawn at the given square is part of a pawn chain
-        file = chess.square_file(square)
-        rank = chess.square_rank(square)
-
-        # Check the adjacent squares
-        for offset in [-1, 1]:
-            neighbor_square = chess.square(file + offset, rank)
-            if board.piece_at(neighbor_square) == chess.Piece(chess.PAWN, board.turn):
-                return True
-
-        return False
-
-    def calculate_pawn_weakness(self, board):
-        # Evaluate pawn weaknesses (isolated pawns and backward pawns)
-        pawn_weakness = 0
-
-        for square in chess.SQUARES:
-            piece = board.piece_at(square)
-            if piece and piece.piece_type == chess.PAWN and piece.color == board.turn:
-                if self.is_isolated_pawn(board, square) or self.is_backward_pawn(board, square):
-                    pawn_weakness += 1
-
-        return pawn_weakness
-
-    def is_isolated_pawn(self, board, square):
-        # Check if the pawn at the given square is isolated
-        file = chess.square_file(square)
-
-        # Check the adjacent files
-        for offset in [-1, 1]:
-            neighbor_square = chess.square(file + offset, chess.square_rank(square))
-            if board.piece_at(neighbor_square) == chess.Piece(chess.PAWN, board.turn):
-                return False
-
-        return True
-
-    def is_backward_pawn(self, board, square):
-        # Check if the pawn at the given square is a backward pawn
-        file = chess.square_file(square)
-        rank = chess.square_rank(square)
-
-        # Check the same file and adjacent files
-        for offset in [-1, 0, 1]:
-            neighbor_square = chess.square(file + offset, rank + 1)
-            if board.piece_at(neighbor_square) == chess.Piece(chess.PAWN, board.turn):
-                return False
-
-        return True
+    def center_pawn_evaluation(self, piece):
+        center_pawn_idx = 0
+        if piece.color == chess.WHITE:
+            self.scores_for_weights[center_pawn_idx][WHITE_SCORE_IDX] += self.weights[center_pawn_idx]
+        else:
+            self.scores_for_weights[center_pawn_idx][BLACK_SCORE_IDX] += self.weights[center_pawn_idx]
