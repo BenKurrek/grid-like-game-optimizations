359c030a0863b79d921ea953a24f420b12f61dbf
BenKurrek <bkurrek@uwaterloo.ca>
2023-11-26 14:17:40 -0500

    added blocked pawns, blocked central pawns, blocked passed pawns, pawn mobility evals

--- src/game/chess/evaluations/pawn.py
+++ src/game/chess/evaluations/pawn.py
@@ -12,7 +12,10 @@ pawn_weight_labels = [
     "Passed Pawn Weight",
     "Pawn Attacking Weight",
     "Pawn Defending Weight",
-    "Pawn number of free spaces",
+    "Pawn Mobility Weight",
+    "Blocked Pawn Weight",
+    "Blocked Passed Pawn Weight",
+    "Blocked Central Pawn Weight",
 ]
 pawn_weight_bounds = [
     # Material
@@ -21,12 +24,12 @@ pawn_weight_bounds = [
     (0, 50), # how much doubled pawns are penalized
     (0, 100), # how much isolated pawns are penalized
     (0, 200), # how much passed pawns are worth
-     # Attacking
-    (0, 1000),
-    # Defending
-    (0, 1000),
-    # Free spaces
-    (0, 1000),   
+    (0, 1000), # how much pawns that attack the enemy king are worth
+    (0, 1000), # how much pawns that defend your king are worth
+    (0, 1000), # how much the mobility of pawns are worth
+    (0, 1000), # how much blocked pawns are penalized
+    (0, 1000), # how much blocked passed pawns are penalized
+    (0, 1000), # how much blocked central pawns are penalized
 ]
 
 WHITE_SCORE_IDX = 0
@@ -49,11 +52,28 @@ class PawnEvaluator:
     def evaluation_for_square(self, square: chess.Square, piece: chess.Piece, attack_squares: list[str]):
         if piece.piece_type == chess.PAWN:
             is_double_pawn, is_passed_pawn = self.check_file_data(square, piece)
+            legal_moves = self.count_legal_pawn_moves(square, piece)
 
             # If the square is a pawn, give it the material bonus
             self.material_evaluation(piece)
+            # If the pawn is attacking the enemy king, give it a bonus
+            # If the pawn is defending the friendly king, give it a bonus
             self.king_attacking_defending_evalutation(attack_squares, square, piece)
-            self.free_squares_evaluation(attack_squares, piece)
+            
+            # Give a bonus for the mobility of the pawn (how many legal moves it has)
+            self.mobility_evaluation(legal_moves, piece)
+
+            # Give a penalty for blocked pawns
+            if legal_moves == 0:
+                self.blocked_pawn_evaluation(piece)
+
+                # Give a penalty for blocked passed pawns
+                if is_passed_pawn:
+                    self.blocked_passed_pawn_evaluation(piece)
+
+                # Give a penalty for blocked central pawns
+                if square in chess.SquareSet(chess.BB_CENTER):
+                    self.blocked_central_pawn_evaluation(piece)
 
             # If the square is in the center, give it a bonus
             if square in chess.SquareSet(chess.BB_CENTER):
@@ -95,6 +115,66 @@ class PawnEvaluator:
         else:
             self.scores_for_weights[double_pawn_idx][BLACK_SCORE_IDX] -= 1
 
+    # Subtract the weight for an isolated pawn
+    def isolated_pawn_evaluation(self, piece):
+        isolated_pawn_idx = 3
+        if piece.color == chess.WHITE:
+            self.scores_for_weights[isolated_pawn_idx][WHITE_SCORE_IDX] -= 1
+        else:
+            self.scores_for_weights[isolated_pawn_idx][BLACK_SCORE_IDX] -= 1
+
+    # Add the weight for a passed pawn
+    def passed_pawn_evaluation(self, square: chess.Square, piece: chess.Piece):
+        if piece.color == chess.WHITE:
+            ranks_until_promotion = 7 - chess.square_rank(square)
+        else:
+            ranks_until_promotion = chess.square_rank(square)
+            
+        passed_pawn_idx = 4
+        if piece.color == chess.WHITE:
+            self.scores_for_weights[passed_pawn_idx][WHITE_SCORE_IDX] += ranks_until_promotion
+        else:
+            self.scores_for_weights[passed_pawn_idx][BLACK_SCORE_IDX] += ranks_until_promotion
+         
+    def king_attacking_defending_evalutation(self, pawn_attack_squares, square: chess.Square, piece: chess.Piece):
+        attacking_pawn_idx = 5
+        defending_pawn_idx = 6
+        
+        attacking_king_squares = self.adjacent_white_king_squares if piece.color == chess.BLACK else self.adjacent_black_king_squares
+        defending_king_squares = self.adjacent_white_king_squares if piece.color == chess.WHITE else self.adjacent_black_king_squares
+        
+        score_idx = WHITE_SCORE_IDX if piece.color is chess.WHITE else BLACK_SCORE_IDX
+        
+        for pawn_attack_square in pawn_attack_squares:
+            # Attacking
+            if pawn_attack_square in attacking_king_squares:
+                self.scores_for_weights[attacking_pawn_idx][score_idx] += 1
+            
+            # Defending   
+            if pawn_attack_square in defending_king_squares:
+                self.scores_for_weights[defending_pawn_idx][score_idx] += 1
+
+    def mobility_evaluation(self, legal_moves, piece: chess.Piece):
+        mobility_idx = 7
+        color_idx = WHITE_SCORE_IDX if piece.color is chess.WHITE else BLACK_SCORE_IDX
+        self.scores_for_weights[mobility_idx][color_idx] += legal_moves
+
+    def blocked_pawn_evaluation(self, piece: chess.Piece):
+        blocked_pawn_idx = 8
+        color_idx = WHITE_SCORE_IDX if piece.color is chess.WHITE else BLACK_SCORE_IDX
+        self.scores_for_weights[blocked_pawn_idx][color_idx] -= 1
+
+    def blocked_passed_pawn_evaluation(self, piece: chess.Piece):
+        blocked_passed_pawn_idx = 9
+        color_idx = WHITE_SCORE_IDX if piece.color is chess.WHITE else BLACK_SCORE_IDX
+        self.scores_for_weights[blocked_passed_pawn_idx][color_idx] -= 1
+
+    def blocked_central_pawn_evaluation(self, piece: chess.Piece):
+        blocked_central_pawn_idx = 10
+        color_idx = WHITE_SCORE_IDX if piece.color is chess.WHITE else BLACK_SCORE_IDX
+        self.scores_for_weights[blocked_central_pawn_idx][color_idx] -= 1
+
+    # UTILITY
     def check_file_data(self, square, piece):
         is_double_pawn = False
         is_passed_pawn = True
@@ -138,14 +218,6 @@ class PawnEvaluator:
 
         return is_double_pawn, is_passed_pawn
 
-    # Subtract the weight for an isolated pawn
-    def isolated_pawn_evaluation(self, piece):
-        isolated_pawn_idx = 3
-        if piece.color == chess.WHITE:
-            self.scores_for_weights[isolated_pawn_idx][WHITE_SCORE_IDX] -= 1
-        else:
-            self.scores_for_weights[isolated_pawn_idx][BLACK_SCORE_IDX] -= 1
-
     def is_isolated_pawn(self, square, piece):
         file = chess.square_file(square)
         rank = chess.square_rank(square)
@@ -170,38 +242,29 @@ class PawnEvaluator:
 
         return True
 
-    # Add the weight for a passed pawn
-    def passed_pawn_evaluation(self, square: chess.Square, piece: chess.Piece):
-        if piece.color == chess.WHITE:
-            ranks_until_promotion = 7 - chess.square_rank(square)
-        else:
-            ranks_until_promotion = chess.square_rank(square)
-            
-        passed_pawn_idx = 4
-        if piece.color == chess.WHITE:
-            self.scores_for_weights[passed_pawn_idx][WHITE_SCORE_IDX] += ranks_until_promotion
-        else:
-            self.scores_for_weights[passed_pawn_idx][BLACK_SCORE_IDX] += ranks_until_promotion
-         
-    def king_attacking_defending_evalutation(self, pawn_attack_squares, square: chess.Square, piece: chess.Piece):
-        attacking_pawn_idx = 5
-        defending_pawn_idx = 6
-        
-        attacking_king_squares = self.adjacent_white_king_squares if piece.color == chess.BLACK else self.adjacent_black_king_squares
-        defending_king_squares = self.adjacent_white_king_squares if piece.color == chess.WHITE else self.adjacent_black_king_squares
-        
-        score_idx = WHITE_SCORE_IDX if piece.color is chess.WHITE else BLACK_SCORE_IDX
-        
-        for pawn_attack_square in pawn_attack_squares:
-            # Attacking
-            if pawn_attack_square in attacking_king_squares:
-                self.scores_for_weights[attacking_pawn_idx][score_idx] += 1
-            
-            # Defending   
-            if pawn_attack_square in defending_king_squares:
-                self.scores_for_weights[defending_pawn_idx][score_idx] += 1
-                
-    def free_squares_evaluation(self, pawn_attack_squares, piece: chess.Piece):
-        free_square_idx = 7
-        score_idx = WHITE_SCORE_IDX if piece.color is chess.WHITE else BLACK_SCORE_IDX
-        self.scores_for_weights[free_square_idx][score_idx] = len(pawn_attack_squares)
+    def count_legal_pawn_moves(self, square, piece):
+        legal_moves = 0
+        direction = 1 if piece.color == chess.WHITE else -1
+
+        # Check one square forward
+        target_square_1 = square + 8 * direction
+        if chess.square_rank(target_square_1) in range(1, 9):
+            if self.board.is_legal(chess.Move(square, target_square_1)):
+                legal_moves += 1
+
+                # Check two squares forward if it's the pawn's first move
+                target_square_2 = square + 16 * direction
+                if (
+                    chess.square_rank(square) in {1, 6}
+                    and self.board.is_legal(chess.Move(square, target_square_2))
+                ):
+                    legal_moves += 1
+
+        # Check captures
+        for file_offset in [-1, 1]:
+            capture_square = square + (8 * direction) + file_offset
+            if chess.square_file(capture_square) in range(1, 9):
+                if self.board.is_legal(chess.Move(square, capture_square)):
+                    legal_moves += 1
+
+        return legal_moves
