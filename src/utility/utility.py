7e1fb368aac154406669c3f127611e9f19b3e5aa
Zach <zach@MacBook-Pro-3.local>
2023-11-26 15:57:32 -0500

    Added create and evaluate game utility

---
+++ src/utility/utility.py
@@ -0,0 +1,26 @@
+from src.game.base_game import BaseGame
+from src.optimization.genetic_algorithm import GeneticAlgorithm
+from src.utility.chess_extraction import extract_random_chess_positions
+from src.utility.othello_extraction import extract_random_othello_positions
+from src.utility.game_chooser import create_base_game
+
+def create_and_evaluate_game(game_name, weights) -> tuple:
+    # Create game
+    board_data = None
+    if game_name == "chess":
+        # Individuals in the population each start with the same random position.
+        # Their chromosomes are made up of genes representing fitness function weights
+        board_data = extract_random_chess_positions(num_positions=1)[0]
+        # Create the population given the set of initial individuals
+    elif game_name == "othello":
+        board_data = extract_random_othello_positions(num_positions=1)[0]
+        
+    game = create_base_game(game_name, board_data)
+    
+    # Set weights
+    game.update_weights(weights)
+    
+    fitness_score, best_move, best_score = game.fitness()
+    index, num_moves = game.rank_move(best_move)
+    
+    return fitness_score, best_move, index, num_moves
